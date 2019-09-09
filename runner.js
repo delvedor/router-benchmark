b4fe82f4c804571af20138e3e2c37fd9420ab1a8
Paris Holley <mail@parisholley.com>
2019-09-08 22:44:51 -0400

    standard fixes

--- runner.js
+++ runner.js
@@ -8,38 +8,38 @@ const chalk = require('chalk')
 const suites = {}
 function noop () {}
 
-const random = [];
+const random = []
 
-for(const route of routes){
-  if(route.path.indexOf('{') > -1){
+for (const route of routes) {
+  if (route.path.indexOf('{') > -1) {
     random.push({
       ...route,
-      path: route.path.replace(/{([A-z]+)}/,'$1')
-    });
+      path: route.path.replace(/{([A-z]+)}/, '$1')
+    })
 
-    for(let i = 0; i < 10; i+= 1){
+    for (let i = 0; i < 10; i += 1) {
       // produce random path names to introduce performance degradation on misses
       const rewrite = {
         ...route,
-        path: route.path.replace(/{([A-z]+)}/,`$1${Math.floor(Math.random() * 10000)}`),
+        path: route.path.replace(/{([A-z]+)}/, `$1${Math.floor(Math.random() * 10000)}`),
         suite: `${route.suite} (404)`
-      };
-
-      if(i === 0 && route.suite){
-        // run a 404 test suite
-        rewrite.suite = `${route.suite} (404)`;
-      }else{
-        delete rewrite.suite;
       }
-      
-      random.push(rewrite);
+
+      if (i === 0 && route.suite) {
+        // run a 404 test suite
+        rewrite.suite = `${route.suite} (404)`
+      } else {
+        delete rewrite.suite
+      }
+
+      random.push(rewrite)
     }
-  }else{
-    random.push(route);
+  } else {
+    random.push(route)
   }
 }
 
-console.log(`\nRegistering ${random.length} routes for each library.`);
+console.log(`\nRegistering ${random.length} routes for each library.`)
 
 fs.readdirSync('./libraries').forEach((f) => {
   const library = require(`./libraries/${f}`)
@@ -64,7 +64,7 @@ const indent = 10
 for (const suiteName in suites) {
   suites[suiteName]
     .on('start', function () {
-      console.log(`\nBenchmarking: ${charl.bold(suiteName)}`)
+      console.log(`\nBenchmarking: ${chalk.bold(suiteName)}`)
     })
     .on('cycle', function (event) {
       console.log(`\t${chalk.gray.italic(String(event.target))}`)
